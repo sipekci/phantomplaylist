66f586c950a5064d5da9997561d9e8197aedfed7
sipekci <senihaipekci@gmail.com>
2022-05-08 18:33:06 -0400

    created Main View File, with Features references

--- phantomplaylist/src/Components/App/App.js
+++ phantomplaylist/src/Components/App/App.js
@@ -1,23 +1,21 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className = "highlight"> Phantom Playlist</h1>
+        <div className ="App">
+          {/* <SearchBar /> */}
+        <div className ="App-playlist">
+          {/* <SearchResults /> */}
+          {/* <Playlist /> */}
+        </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
